d17f3c86364e256e89d1e6bab12db9024d4d136f
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-03-03 18:17:11 -0500

    made some small changes, added phone_number

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,6 +5,7 @@ const signupFormHandler = async (event) => {
     const last_name = document.querySelector('#lastName').value.trim();
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
+    const phone_number = document.querySelector('#phoneNumber').value.trim();
     const question1 = document.querySelector('#question1').value.trim();
     const answer1 = document.querySelector('#answer1').value.trim();
     const question2 = document.querySelector('#question2').value.trim();
@@ -25,7 +26,7 @@ const signupFormHandler = async (event) => {
     if (email && password) {
         const response = await fetch('/api/user/signup', {
             method: 'POST',
-            body: JSON.stringify({ first_name, last_name, email, password, question1, answer1, question2, answer2, question3, answer3 }),
+            body: JSON.stringify({ first_name, last_name, email, password, phone_number, question1, answer1, question2, answer2, question3, answer3 }),
             headers: { 'Content-Type': 'application/json' },
         });
 
--- seeds/userData.js
+++ seeds/userData.js
@@ -6,25 +6,29 @@ const userdata = [
         first_name: 'Suvarna',
         last_name: 'Jadhav',
         email: 'suva@gmail.com',
-        password: 'suva'
+        password: 'suva',
+        phone_number: '2342342345'
     },
     {
         first_name: 'Brian',
         last_name: 'McDonell',
         email: 'brian@gmail.com',
-        password: 'brian'
+        password: 'brian',
+        phone_number: '5432342345'
     },
     {
         first_name: 'Pallavi',
         last_name: 'Talekar',
         email: 'talekar@gmail.com',
-        password: 'pallu'
+        password: 'pallu',
+        phone_number: '8762342345'
     },
     {
         first_name: 'Aditi',
         last_name: 'Pimple',
         email: 'pimple@gmail.com',
-        password: 'aditi'
+        password: 'aditi',
+        phone_number: '9872342345'
     }
 ];
 
